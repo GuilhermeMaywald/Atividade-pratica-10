9874fc370ca21cb36a0239347f0433b567de228e
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-12 21:25:07 -0300

    Atividade pratica 10 JS

---
+++ js/objetos.js
@@ -0,0 +1,20 @@
+{
+    let apartamento = {
+        salas: {
+            salaEstar: 1,
+            salaJantar: 1,
+            salaDeJogos: 1
+        },
+        cozinha: 1,
+        copa: 1,
+        quartos: {
+            quartoSuite: 1,
+            quartoInfantil: 2,
+            quartoCasal: 1
+        },
+        escritorio: 1,
+        quintal: 1
+    };
+    console.log(apartamento);
+    console.log(apartamento.salas);
+}
